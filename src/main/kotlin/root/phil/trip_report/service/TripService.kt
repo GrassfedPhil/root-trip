d3869163f2947d00b1524f880a55daf9131496b6
Phil <parlihbp@gmail.com>
2020-09-01 17:04:07 -0400

    separating concerns into different classes.

---
+++ src/main/kotlin/root/phil/trip_report/service/TripService.kt
@@ -0,0 +1,32 @@
+package root.phil.trip_report.service
+
+import org.springframework.stereotype.Service
+import root.phil.trip_report.model.Driver
+import root.phil.trip_report.model.Trip
+import java.io.File
+import java.time.LocalTime
+import java.time.temporal.ChronoUnit.MINUTES
+
+@Service
+class TripService {
+
+    fun createDriverMap(commandLines: List<String>): Map<String, Driver> {
+        val (drivers, trips) = commandLines.partition { line -> line.startsWith("Driver") }
+        val driversMap = drivers.map { Driver(it.split(" ").last()) }.associateBy { it.driverName }
+        trips.filter { it.startsWith("Trip") }.map { tripString ->
+            tripString.split(" ")
+                    .let { (_, driverName, startTime, endTime, milesDriven) ->
+                        val minutesBetween = MINUTES.between(LocalTime.parse(startTime), LocalTime.parse(endTime))
+                        val mph = milesDriven.toDouble() / minutesBetween.toDouble() * 60
+                        mph.takeIf { it >= 5 && it < 100}?.let {
+                            Trip(LocalTime.parse(startTime), LocalTime.parse(endTime), milesDriven.toDouble(), mph)
+                        }?.run {
+                            driversMap[driverName]?.trips?.add(this)
+                        }
+                    }
+
+        }
+        return driversMap
+    }
+
+}
--- src/test/kotlin/root/phil/trip_report/service/CommandParserTest.kt
+++ src/test/kotlin/root/phil/trip_report/service/CommandParserTest.kt
@@ -1,29 +1,21 @@
 package root.phil.trip_report.service
 
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.entry
-import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.io.ResourceLoader
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import root.phil.trip_report.model.Driver
-import root.phil.trip_report.model.Trip
-import java.time.LocalTime
-import java.util.function.BiPredicate
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
 internal class CommandParserTest {
-
     @Autowired
     lateinit var resourceLoader: ResourceLoader
 
     @Autowired
     lateinit var commandParser: CommandParser
-
 
     @Test
     fun `parser will create array of commands for each line in file`() {
@@ -32,80 +24,5 @@ internal class CommandParserTest {
 
         assertThat(lines).containsExactly("hello", "there", "i'm some", "commands")
 
-    }
-
-    @Test
-    fun `parser will create driver map from file`() {
-        val driverMap = commandParser.createDriverMap(listOf("Driver Dave", "Driver Chris"))
-        assertThat(driverMap).containsOnly(
-                entry("Dave", Driver("Dave")),
-                entry("Chris", Driver("Chris"))
-        )
-    }
-
-    @Test
-    fun `parser will ignore unknown commands and not explode`() {
-        val driverMap = commandParser.createDriverMap(listOf("hello", "there"))
-
-        assertThat(driverMap).isEmpty()
-    }
-
-    final var closeEnough = BiPredicate { d1: Double, d2: Double -> Math.abs(d1 - d2) <= .05 }
-    var configuration: RecursiveComparisonConfiguration = RecursiveComparisonConfiguration.builder()
-            .withEqualsForType(closeEnough, Double::class.javaObjectType)
-            .build()
-
-    @Test
-    fun `parser will add add trips to recognized drivers`() {
-        val driverMap = commandParser.createDriverMap(listOf(
-                "Driver Dave",
-                "Driver Chris",
-                "Trip Dave 11:11 12:34 55.6",
-                "Trip Dave 13:00 16:34 100.6"
-        ))
-
-        val dave = driverMap["Dave"]
-        val chris = driverMap["Chris"]
-        assertThat(dave?.driverName).isEqualTo("Dave")
-        assertThat(dave?.trips).usingRecursiveFieldByFieldElementComparator(configuration)
-                .containsOnly(
-                        Trip(LocalTime.parse("11:11"), LocalTime.parse("12:34"), 55.6, 40.19),
-                        Trip(LocalTime.parse("13:00"), LocalTime.parse("16:34"), 100.6, 28.20)
-                )
-        assertThat(chris?.driverName).isEqualTo("Chris")
-        assertThat(chris?.trips).isEmpty()
-
-    }
-
-    @Test
-    fun `parser will not add trips that have an avg speed of less than 5mph`() {
-        val driverMap = commandParser.createDriverMap(listOf(
-                "Driver Dave",
-                "Trip Dave 11:11 12:34 55.6",
-                "Trip Dave 13:00 14:00 4.9",
-                "Trip Dave 14:00 15:00 5"
-        ))
-
-        assertThat(driverMap["Dave"]?.trips).usingRecursiveFieldByFieldElementComparator(configuration)
-                .containsOnly(
-                        Trip(LocalTime.parse("11:11"), LocalTime.parse("12:34"), 55.6, 40.19),
-                        Trip(LocalTime.parse("14:00"), LocalTime.parse("15:00"), 5.0, 5.0)
-                )
-    }
-
-    @Test
-    fun `parser will not add trips that have an avg speed of greater than or equal 60mph`() {
-        val driverMap = commandParser.createDriverMap(listOf(
-                "Driver Dave",
-                "Trip Dave 11:11 12:34 55.6",
-                "Trip Dave 14:00 15:01 100",
-                "Trip Dave 15:00 15:59 100"
-        ))
-
-        assertThat(driverMap["Dave"]?.trips).usingRecursiveFieldByFieldElementComparator(configuration)
-                .containsOnly(
-                        Trip(LocalTime.parse("11:11"), LocalTime.parse("12:34"), 55.6, 40.19),
-                        Trip(LocalTime.parse("14:00"), LocalTime.parse("15:01"), 100.0, 98.36)
-                )
     }
 }
