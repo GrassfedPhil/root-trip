e8a9d4a7edb2e82b1a02b807e8c4687420648c52
Phil <parlihbp@gmail.com>
2020-09-02 21:37:11 -0400

    refactoring

--- src/main/kotlin/root/phil/trip_report/service/ReportService.kt
+++ src/main/kotlin/root/phil/trip_report/service/ReportService.kt
@@ -8,13 +8,16 @@ import kotlin.math.roundToInt
 @Service
 class ReportService {
     fun generateReport(driverMap: Map<String, Driver>): List<String> {
-
         return driverMap.values.sortedByDescending { it.trips.sumBy { trip -> trip.distance.roundToInt() } }
                 .map { driver ->
                     val distance = driver.trips.sumBy { it.distance.roundToInt() }
                     val totalTime = driver.trips.sumBy { ChronoUnit.MINUTES.between(it.startTime, it.endTime).toInt() }
-                    val mph = distance.toDouble() / totalTime.toDouble() * 60
+                    val mph = distance.toDouble() / totalTime.toDouble() * SIXTY_MINUTES
                     "${driver.driverName}: $distance miles${if (mph > 0) " @ ${mph.roundToInt()} mph" else ""}"
                 }
+    }
+
+    companion object {
+        const val SIXTY_MINUTES = 60
     }
 }
--- src/main/kotlin/root/phil/trip_report/service/TripService.kt
+++ src/main/kotlin/root/phil/trip_report/service/TripService.kt
@@ -3,7 +3,6 @@ package root.phil.trip_report.service
 import org.springframework.stereotype.Service
 import root.phil.trip_report.model.Driver
 import root.phil.trip_report.model.Trip
-import java.io.File
 import java.time.LocalTime
 import java.time.temporal.ChronoUnit.MINUTES
 
@@ -13,20 +12,29 @@ class TripService {
     fun createDriverMap(commandLines: List<String>): Map<String, Driver> {
         val (drivers, trips) = commandLines.partition { line -> line.startsWith("Driver") }
         val driversMap = drivers.map { Driver(it.split(" ").last()) }.associateBy { it.driverName }
+        associateTripsToDrivers(trips, driversMap)
+        return driversMap
+    }
+
+    private fun associateTripsToDrivers(trips: List<String>, driversMap: Map<String, Driver>) {
         trips.filter { it.startsWith("Trip") }.map { tripString ->
             tripString.split(" ")
                     .let { (_, driverName, startTime, endTime, milesDriven) ->
                         val minutesBetween = MINUTES.between(LocalTime.parse(startTime), LocalTime.parse(endTime))
-                        val mph = milesDriven.toDouble() / minutesBetween.toDouble() * 60
-                        mph.takeIf { it >= 5 && it < 100}?.let {
+                        val mph = milesDriven.toDouble() / minutesBetween.toDouble() * SIXTY_MINUTES
+                        mph.takeIf { it >= LOWER_MPH_LIMIT && it < UPPER_MPH_LIMIT }?.let {
                             Trip(LocalTime.parse(startTime), LocalTime.parse(endTime), milesDriven.toDouble(), mph)
                         }?.run {
                             driversMap[driverName]?.trips?.add(this)
                         }
                     }
-
         }
-        return driversMap
+    }
+
+    companion object {
+        const val LOWER_MPH_LIMIT = 5
+        const val UPPER_MPH_LIMIT = 100
+        const val SIXTY_MINUTES = 60
     }
 
 }
